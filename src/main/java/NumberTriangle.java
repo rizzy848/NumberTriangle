1047f2f12b90c096f4f13f1d865a8920a558e103
gskahlon <gurmanatsinghkahlon@gmail.com>
2025-10-02 18:43:56 -0400

    Implement loadTriangle(fname) to build NumberTriangle from resource file

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
@@ -88,7 +89,7 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        if(path == ""){
+        if("".equals(path)){
             return root;
         }
         else{
@@ -122,8 +123,7 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-
-        // TODO define any variables that you want to use to store things
+        ArrayList<NumberTriangle> prevRow = new ArrayList<>();
 
         // will need to return the top of the NumberTriangle,
         // so might want a variable for that.
@@ -133,9 +133,26 @@ public class NumberTriangle {
         while (line != null) {
 
             // remove when done; this line is included so running starter code prints the contents of the file
-            System.out.println(line);
+            //System.out.println(line);
 
-            // TODO process the line
+            String[] nums = line.trim().split("\\s+");
+            ArrayList<NumberTriangle> currRow = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                int val = Integer.parseInt(nums[i]);
+                NumberTriangle node = new NumberTriangle(val);
+                currRow.add(node);
+                // link parent(s) in previous row, if not the first row
+                if (!prevRow.isEmpty()) {
+                    if (i < prevRow.size())
+                        prevRow.get(i).setLeft(node);
+                    if (i > 0)
+                        prevRow.get(i - 1).setRight(node);
+                }
+            }
+            prevRow = currRow;
+            if (top == null && !currRow.isEmpty()) {
+                top = currRow.get(0);
+            }
 
             //read the next line
             line = br.readLine();
